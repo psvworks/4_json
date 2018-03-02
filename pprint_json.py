3470e559469f33ef55b935a62c3a7fdfd8394b76
unknown <ProkhorenkovSV@sergeycomp.Sibdorstroy.local>
2018-03-02 11:27:18 +0500

    изменена функция загрузки json

--- pprint_json.py
+++ pprint_json.py
@@ -1,19 +1,15 @@
 import json
+import sys
 
 
 def load_data(filepath):
     with open(filepath, encoding='utf8') as json_data:
-        return json_data.read()
+        return json.load(json_data.read())
 
 
 def pretty_print_json(data):
-    j = json.loads(data)
-    print(j)
-    #print(json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True))
-    pass
+    return json.dumps(data, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    #p = "https:\\devman.org\media\filer_public\1d\32\1d32132e-efa4-4a6c-bd32-312acc3710ad\alco_shops.json"
-    p = "alco_shops.json"
-    pretty_print_json(load_data(p))
+    pretty_print_json(load_data(sys.argv[1]))
