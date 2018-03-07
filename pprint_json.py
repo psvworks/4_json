2d1c50a3de6d2cac6de37247b298f302b32a0fe5
unknown <ProkhorenkovSV@sergeycomp.Sibdorstroy.local>
2018-03-07 11:23:49 +0500

    Опять изменены имена переменных

--- pprint_json.py
+++ pprint_json.py
@@ -2,15 +2,14 @@ import json
 import sys
 
 
-def load_json(json_file):
-    with open(json_file, encoding='utf8') as jdata:
-        return json.loads(jdata.read())
+def load_json(file_with_json):
+    with open(file_with_json, encoding='utf8') as json_string:
+        return json.loads(json_string.read())
 
 
 def print_json(json_data):
-    j = json.dumps(json_data, indent=4, ensure_ascii=False)
-    print(j)
-    return j
+    print(json.dumps(json_data, indent=4, ensure_ascii=False))
+    return 0
 
 
 if __name__ == '__main__':
